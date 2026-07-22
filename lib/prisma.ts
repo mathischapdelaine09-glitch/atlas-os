fdb8e1ca2ae2ca6449adba30b3770e216fb435e3
mathischapdelaine09-glitch <mathis.chapdelaine09@gmail.com>
2026-07-22 21:39:27 +0200

    Fix prisma deployment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const libsql = createClient({
-  url: "file:./prisma/dev.db",
-});
-
-// @ts-ignore - Bypass temporaire si les types Prisma 7 s'emmêlent les pinceaux
-const adapter = new PrismaLibSQL(libsql);
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
